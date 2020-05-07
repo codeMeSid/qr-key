d65a9b48687a6db8d9f19f45f1a049e7a67be120
codeMeSid <33578140+codeMeSid@users.noreply.github.com>
2020-05-07 21:05:19 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+if (!process.env.NODE_ENV) require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
+
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "client", "build")));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(helmet());
+
+app.get("*", (_, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`listening to ${process.env.PORT}`)
+);
