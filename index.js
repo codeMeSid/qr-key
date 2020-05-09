ea25b5c50a7e47af4a7a766feac04e017f34cba0
codemesid <siddhanthbajoria@gmail.com>
2020-05-09 16:39:56 +0530

    tracy map updated with createdAt

--- index.js
+++ index.js
@@ -26,8 +26,8 @@ app.post("/api/sign", (req, res) => {
   const keyB = jwt.sign(msg, "admin");
   const keyCombo = keyA + "+" + keyB;
   const token = jwt.sign(keyCombo, "I_AM_BATMAN");
-  const tokenId = uuid.v4().split("-")[2];
-  tracy[tokenId] = token;
+  const tokenId = uuid.v4().split("-")[3];
+  tracy[tokenId] = { token, createdAt: Date.now().valueOf() };
   console.log(tracy);
   res.json(tokenId);
 });
